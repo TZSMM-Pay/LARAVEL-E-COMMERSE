78e9f6a12c22a544ff4bac12e0ff99386446ecf9
tzsmm <techzonesmm@gmail.com>
2025-02-21 14:54:10 +0600

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,6 @@
+INSERT INTO `payment_gateways` (`id`, `key`, `value`, `created_at`, `updated_at`) 
+VALUES 
+(NULL, 'tzsmmpay_charge', '0', NOW(), NOW()),
+(NULL, 'tzsmmpay_api_key', '0', NOW(), NOW()),
+(NULL, 'tzsmmpay_image', '0', NOW(), NOW()),
+(NULL, 'tzsmmpay_status', '0', NOW(), NOW());

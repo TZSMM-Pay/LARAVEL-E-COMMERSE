3b008a05b236da21f563cba4b914bc92e45357d9
tzsmm <techzonesmm@gmail.com>
2025-02-21 14:57:02 +0600

    Create navbar.blade.php

---
+++ Modules/PaymentGateway/resources/views/tabs/navbar.blade.php
@@ -0,0 +1,4 @@
+<li class="nav-item">
+    <a class="nav-link " id="tzsmmpay-tab" data-toggle="tab" href="#tzsmmpay_tab" role="tab"
+        aria-controls="tzsmmpay" aria-selected="true">{{ __('TZSMM Pay') }}</a>
+</li>
